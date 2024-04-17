52ba8fe8cffa8bec96126cb7b88ecf45c168161d
Ilya Kushlianski <kushliansky@gmail.com>
2024-04-17 17:36:46 +0300

    Add initial roles
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

---
+++ db-schema/initial-data.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.roles
+(role_id, role_name)
+VALUES('01HY8M06AJY1J6VCZEX9EYZ490', 'Admin');
+INSERT INTO public.roles
+(role_id, role_name)
+VALUES('01HY8M15TWEWB4WAKJF7Z2V34B', 'Manager');
+INSERT INTO public.roles
+(role_id, role_name)
+VALUES('01HY8M1E76BRQRQ6EK22DT1D7G', 'Mentor');
+INSERT INTO public.roles
+(role_id, role_name)
+VALUES('01HY8M21YZZFP49SSMRWSB41XJ', 'Student');
+INSERT INTO public.roles
+(role_id, role_name)
+VALUES('01HY8M2NTKA6SZGADNV5G59B2B', 'External Developer');
