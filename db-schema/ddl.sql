58b33e16e04d3e7429c46ad2146ca33573af4420
Ilya Kushlianski <kushliansky@gmail.com>
2024-04-21 10:49:51 +0300

    Add tuition languages
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- db-schema/ddl.sql
+++ db-schema/ddl.sql
@@ -1,26 +1,4 @@
--- DROP SCHEMA public;
-
-CREATE SCHEMA public AUTHORIZATION ikushlianski;
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
--- Drop table
-
--- DROP TABLE public.classes;
-
-CREATE TABLE public.classes (
-	class_id varchar NOT NULL,
-	class_code varchar NULL, -- Should be generated automatically based on the course (path) code plus date
-	path_id varchar NULL,
-	start_date varchar NOT NULL,
-	end_date varchar NULL,
-	CONSTRAINT classes_pk PRIMARY KEY (class_id),
-	CONSTRAINT classes_paths_fk FOREIGN KEY (path_id) REFERENCES public.paths(path_id)
-);
-
--- Column comments
-
-COMMENT ON COLUMN public.classes.class_code IS 'Should be generated automatically based on the course (path) code plus date';
+-- public.clients definition
 
 -- Drop table
 
@@ -36,6 +14,9 @@ CREATE TABLE public.clients (
 
 COMMENT ON COLUMN public.clients.client_id IS 'Company that bought our services';
 
+
+-- public.companies definition
+
 -- Drop table
 
 -- DROP TABLE public.companies;
@@ -46,6 +27,208 @@ CREATE TABLE public.companies (
 	company_location varchar NOT NULL,
 	CONSTRAINT companies_pk PRIMARY KEY (company_id)
 );
+
+
+-- public.lessons definition
+
+-- Drop table
+
+-- DROP TABLE public.lessons;
+
+CREATE TABLE public.lessons (
+	lesson_id varchar NOT NULL,
+	lesson_name varchar NOT NULL,
+	CONSTRAINT lessons_pk PRIMARY KEY (lesson_id)
+);
+
+
+-- public.mentors definition
+
+-- Drop table
+
+-- DROP TABLE public.mentors;
+
+CREATE TABLE public.mentors (
+	mentor_id varchar NOT NULL,
+	user_id varchar NOT NULL,
+	mentor_full_name_ru varchar NOT NULL,
+	mentor_full_name_en varchar NOT NULL,
+	mentor_description_ru varchar NULL,
+	mentor_description_en varchar NULL,
+	CONSTRAINT mentors_pk PRIMARY KEY (mentor_id)
+);
+
+
+-- public.modules definition
+
+-- Drop table
+
+-- DROP TABLE public.modules;
+
+CREATE TABLE public.modules (
+	module_id varchar NOT NULL,
+	module_name varchar NOT NULL,
+	CONSTRAINT modules_pk PRIMARY KEY (module_id)
+);
+
+
+-- public.paths_levels definition
+
+-- Drop table
+
+-- DROP TABLE public.paths_levels;
+
+CREATE TABLE public.paths_levels (
+	path_level_id varchar NOT NULL,
+	path_level_name varchar NOT NULL, -- E.g. for English studies this can be "B1", "C1" etc.
+	CONSTRAINT paths_levels_pk PRIMARY KEY (path_level_id)
+);
+
+-- Column comments
+
+COMMENT ON COLUMN public.paths_levels.path_level_name IS 'E.g. for English studies this can be "B1", "C1" etc.';
+
+
+-- public.paths_mentors definition
+
+-- Drop table
+
+-- DROP TABLE public.paths_mentors;
+
+CREATE TABLE public.paths_mentors (
+	path_id varchar NOT NULL,
+	mentor_id varchar NOT NULL
+);
+COMMENT ON TABLE public.paths_mentors IS 'Default mentors for paths, may be changed once a class is created from path';
+
+
+-- public.positions definition
+
+-- Drop table
+
+-- DROP TABLE public.positions;
+
+CREATE TABLE public.positions (
+	position_id varchar NOT NULL,
+	position_name varchar NOT NULL,
+	CONSTRAINT positions_pk PRIMARY KEY (position_id)
+);
+
+
+-- public.prizes definition
+
+-- Drop table
+
+-- DROP TABLE public.prizes;
+
+CREATE TABLE public.prizes (
+	prize_id varchar NOT NULL,
+	prize_name varchar NULL,
+	CONSTRAINT prizes_pk PRIMARY KEY (prize_id)
+);
+
+
+-- public.roles definition
+
+-- Drop table
+
+-- DROP TABLE public.roles;
+
+CREATE TABLE public.roles (
+	role_id varchar NOT NULL,
+	role_name varchar NULL,
+	CONSTRAINT roles_pk PRIMARY KEY (role_id)
+);
+
+
+-- public.sections definition
+
+-- Drop table
+
+-- DROP TABLE public.sections;
+
+CREATE TABLE public.sections (
+	section_id varchar NOT NULL,
+	section_name varchar NOT NULL,
+	CONSTRAINT sections_pk PRIMARY KEY (section_id)
+);
+
+
+-- public.student_class_ranking definition
+
+-- Drop table
+
+-- DROP TABLE public.student_class_ranking;
+
+CREATE TABLE public.student_class_ranking (
+	student_id varchar NOT NULL,
+	class_id varchar NOT NULL,
+	points int4 NULL DEFAULT 0
+);
+
+
+-- public.students definition
+
+-- Drop table
+
+-- DROP TABLE public.students;
+
+CREATE TABLE public.students (
+	student_id varchar NOT NULL,
+	class_id varchar NOT NULL,
+	user_id varchar NOT NULL,
+	CONSTRAINT students_pk PRIMARY KEY (student_id)
+);
+
+
+-- public.subjects definition
+
+-- Drop table
+
+-- DROP TABLE public.subjects;
+
+CREATE TABLE public.subjects (
+	subject_id varchar NOT NULL,
+	subject_code varchar NOT NULL,
+	subject_name varchar NOT NULL,
+	CONSTRAINT subjects_pk PRIMARY KEY (subject_id)
+);
+
+
+-- public.users definition
+
+-- Drop table
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users (
+	user_id varchar NOT NULL,
+	email varchar NULL,
+	github_username varchar NULL,
+	first_name varchar NULL,
+	last_name varchar NULL,
+	linkedin_url varchar NULL,
+	CONSTRAINT users_pk PRIMARY KEY (user_id),
+	CONSTRAINT users_unique UNIQUE (email),
+	CONSTRAINT users_unique_1 UNIQUE (github_username)
+);
+
+
+-- public.tuition_languages definition
+
+-- Drop table
+
+-- DROP TABLE public.tuition_languages;
+
+CREATE TABLE public.tuition_languages (
+	tuition_lang_id varchar NOT NULL,
+	tuition_lang_name varchar NOT NULL,
+	tuition_lang_code varchar NOT NULL,
+	CONSTRAINT tuition_languages_pk PRIMARY KEY (tuition_lang_id)
+);
+
+
+-- public.companies_users definition
 
 -- Drop table
 
@@ -60,36 +243,8 @@ CREATE TABLE public.companies_users (
 	CONSTRAINT companies_users_users_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id)
 );
 
--- Drop table
 
--- DROP TABLE public.lessons;
-
-CREATE TABLE public.lessons (
-	lesson_id varchar NOT NULL,
-	lesson_name varchar NOT NULL,
-	CONSTRAINT lessons_pk PRIMARY KEY (lesson_id)
-);
-
--- Drop table
-
--- DROP TABLE public.mentors;
-
-CREATE TABLE public.mentors (
-	user_id varchar NOT NULL,
-	class_id varchar NOT NULL,
-	CONSTRAINT mentors_classes_fk FOREIGN KEY (class_id) REFERENCES public.classes(class_id),
-	CONSTRAINT mentors_users_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id)
-);
-
--- Drop table
-
--- DROP TABLE public.modules;
-
-CREATE TABLE public.modules (
-	module_id varchar NOT NULL,
-	module_name varchar NOT NULL,
-	CONSTRAINT modules_pk PRIMARY KEY (module_id)
-);
+-- public.paths definition
 
 -- Drop table
 
@@ -109,19 +264,8 @@ CREATE TABLE public.paths (
 
 COMMENT ON COLUMN public.paths.path_code IS 'Code of the path user internally for automation. E.g. fe-angular-b, meaning frontend angular beginners';
 
--- Drop table
 
--- DROP TABLE public.paths_levels;
-
-CREATE TABLE public.paths_levels (
-	path_level_id varchar NOT NULL,
-	path_level_name varchar NOT NULL, -- E.g. for English studies this can be "B1", "C1" etc.
-	CONSTRAINT paths_levels_pk PRIMARY KEY (path_level_id)
-);
-
--- Column comments
-
-COMMENT ON COLUMN public.paths_levels.path_level_name IS 'E.g. for English studies this can be "B1", "C1" etc.';
+-- public.paths_structures definition
 
 -- Drop table
 
@@ -138,25 +282,8 @@ CREATE TABLE public.paths_structures (
 	CONSTRAINT paths_structures_sections_fk FOREIGN KEY (section_id) REFERENCES public.sections(section_id)
 );
 
--- Drop table
 
--- DROP TABLE public.positions;
-
-CREATE TABLE public.positions (
-	position_id varchar NOT NULL,
-	position_name varchar NOT NULL,
-	CONSTRAINT positions_pk PRIMARY KEY (position_id)
-);
-
--- Drop table
-
--- DROP TABLE public.prizes;
-
-CREATE TABLE public.prizes (
-	prize_id varchar NOT NULL,
-	prize_name varchar NULL,
-	CONSTRAINT prizes_pk PRIMARY KEY (prize_id)
-);
+-- public.projects definition
 
 -- Drop table
 
@@ -171,6 +298,9 @@ CREATE TABLE public.projects (
 	CONSTRAINT projects_clients_fk FOREIGN KEY (client_id) REFERENCES public.clients(client_id)
 );
 
+
+-- public.raffles definition
+
 -- Drop table
 
 -- DROP TABLE public.raffles;
@@ -182,47 +312,8 @@ CREATE TABLE public.raffles (
 	CONSTRAINT raffles_prizes_fk FOREIGN KEY (prize_id) REFERENCES public.prizes(prize_id)
 );
 
--- Drop table
 
--- DROP TABLE public.roles;
-
-CREATE TABLE public.roles (
-	role_id varchar NOT NULL,
-	role_name varchar NULL,
-	CONSTRAINT roles_pk PRIMARY KEY (role_id)
-);
-
--- Drop table
-
--- DROP TABLE public.sections;
-
-CREATE TABLE public.sections (
-	section_id varchar NOT NULL,
-	section_name varchar NOT NULL,
-	CONSTRAINT sections_pk PRIMARY KEY (section_id)
-);
-
--- Drop table
-
--- DROP TABLE public.students;
-
-CREATE TABLE public.students (
-	student_id varchar NOT NULL,
-	class_id varchar NOT NULL,
-	user_id varchar NOT NULL,
-	CONSTRAINT students_pk PRIMARY KEY (student_id)
-);
-
--- Drop table
-
--- DROP TABLE public.subjects;
-
-CREATE TABLE public.subjects (
-	subject_id varchar NOT NULL,
-	subject_code varchar NOT NULL,
-	subject_name varchar NOT NULL,
-	CONSTRAINT subjects_pk PRIMARY KEY (subject_id)
-);
+-- public.user_roles definition
 
 -- Drop table
 
@@ -235,21 +326,8 @@ CREATE TABLE public.user_roles (
 	CONSTRAINT user_roles_users_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id)
 );
 
--- Drop table
 
--- DROP TABLE public.users;
-
-CREATE TABLE public.users (
-	user_id varchar NOT NULL,
-	email varchar NULL,
-	github_username varchar NULL,
-	first_name varchar NULL,
-	last_name varchar NULL,
-	linkedin_url varchar NULL,
-	CONSTRAINT users_pk PRIMARY KEY (user_id),
-	CONSTRAINT users_unique UNIQUE (email),
-	CONSTRAINT users_unique_1 UNIQUE (github_username)
-);
+-- public.users_projects definition
 
 -- Drop table
 
@@ -262,3 +340,40 @@ CREATE TABLE public.users_projects (
 	CONSTRAINT users_projects_users_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id)
 );
 CREATE UNIQUE INDEX users_projects_user_id_idx ON public.users_projects USING btree (user_id, project_id);
+
+
+-- public.classes definition
+
+-- Drop table
+
+-- DROP TABLE public.classes;
+
+CREATE TABLE public.classes (
+	class_id varchar NOT NULL,
+	class_code varchar NULL, -- Should be generated automatically based on the course (path) code plus date
+	path_id varchar NULL,
+	start_date varchar NOT NULL,
+	end_date varchar NULL,
+	tuition_lang_id varchar NOT NULL,
+	CONSTRAINT classes_pk PRIMARY KEY (class_id),
+	CONSTRAINT classes_paths_fk FOREIGN KEY (path_id) REFERENCES public.paths(path_id),
+	CONSTRAINT classes_tuition_languages_fk FOREIGN KEY (tuition_lang_id) REFERENCES public.tuition_languages(tuition_lang_id)
+);
+
+-- Column comments
+
+COMMENT ON COLUMN public.classes.class_code IS 'Should be generated automatically based on the course (path) code plus date';
+
+
+-- public.classes_mentors definition
+
+-- Drop table
+
+-- DROP TABLE public.classes_mentors;
+
+CREATE TABLE public.classes_mentors (
+	class_id varchar NOT NULL,
+	mentor_id varchar NOT NULL,
+	CONSTRAINT classes_mentors_classes_fk FOREIGN KEY (class_id) REFERENCES public.classes(class_id),
+	CONSTRAINT classes_mentors_mentors_fk FOREIGN KEY (mentor_id) REFERENCES public.mentors(mentor_id)
+);
--- db-schema/initial-data.sql
+++ db-schema/initial-data.sql
@@ -1,3 +1,4 @@
+-- Roles
 INSERT INTO public.roles
 (role_id, role_name)
 VALUES('01HY8M06AJY1J6VCZEX9EYZ490', 'Admin');
@@ -13,3 +14,17 @@ VALUES('01HY8M21YZZFP49SSMRWSB41XJ', 'Student');
 INSERT INTO public.roles
 (role_id, role_name)
 VALUES('01HY8M2NTKA6SZGADNV5G59B2B', 'External Developer');
+
+-- Tuition languages
+INSERT INTO public.tuition_languages
+(tuition_lang_id, tuition_lang_name, tuition_lang_code)
+VALUES('01HY9EVCTXEV9W1Z67YEPS3A2C', 'Русский', 'ru');
+INSERT INTO public.tuition_languages
+(tuition_lang_id, tuition_lang_name, tuition_lang_code)
+VALUES('01HY9EY23SRBH5F0X06TG98WYH', 'English', 'en');
+INSERT INTO public.tuition_languages
+(tuition_lang_id, tuition_lang_name, tuition_lang_code)
+VALUES('01HY9EYDRTKWHP5XD8WCQFYGBK', 'Polska', 'pl');
+INSERT INTO public.tuition_languages
+(tuition_lang_id, tuition_lang_name, tuition_lang_code)
+VALUES('01HY9EZGZ7H141RANT1D8AN6MD', 'Українська', 'ukr');
