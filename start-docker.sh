5b55ef2fb5ae1ddce23c2eb960636d267e8eb0a8
Makarov Sergey <sgmakgg@gmail.com>
2024-08-29 16:38:56 +0300

    start-docker.sh updated
    Signed-off-by: Makarov Sergey <sgmakgg@gmail.com>

--- start-docker.sh
+++ start-docker.sh
@@ -16,4 +16,4 @@ fi
 export ENV_FILE
 
 # Run docker-compose with the specified .env file
-docker-compose --env-file "$ENV_FILE" up --remove-orphans
+docker compose --env-file "$ENV_FILE" up --remove-orphans
