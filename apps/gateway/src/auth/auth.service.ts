5575ecefa06123571d01bd2fe38320f3aefa1e22
Natalya Baranova <byasha29400@gmail.com>
2024-09-04 22:45:17 +0300

    SP-21: Record user to DB upon signup

--- apps/gateway/src/auth/auth.service.ts
+++ apps/gateway/src/auth/auth.service.ts
@@ -1,28 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ParsedUserData } from './auth.types';
+import { PrismaService } from '../prisma/prisma.service';  // Using Prisma ORM
+import { User } from '@prisma/client'; // Importing User Model
+import * as cookie from 'cookie';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private prisma: PrismaService,
+  ) {}
 
   async generateJwtToken(user: ParsedUserData) {
+    const appDomain = process.env.APP_DOMAIN;
+
     if (!user) {
       throw new Error('User not found');
     }
 
-    //    .... your business logic
+    let dbUser = await this.prisma.user.findUnique({
+      where: { email: user.email },
+      include: { roles: true },
+    });
 
-    //   .... add whatever payload you want to have
+    if (!dbUser) {
+      dbUser = await this.prisma.user.create({
+        data: {
+          email: user.email,
+          name: user.name,
+          lastLoginTime: new Date(),
+          privacyPolicyAccepted: false,  
+          roles: {
+            connectOrCreate: {
+              where: { name: 'Spectator' },
+              create: { name: 'Spectator' },
+            },
+          },
+        },
+        include: { roles: true },
+      });
+
+      throw new UnauthorizedException('User must accept the privacy policy.');
+    }
+
+    if (!dbUser.privacyPolicyAccepted) {
+      throw new UnauthorizedException('User must accept the privacy policy.');
+    }
+
+    await this.prisma.user.update({
+      where: { email: dbUser.email },
+      data: {
+        lastLoginTime: new Date(),
+      },
+    });
+    
+
     const payload = {
       email: user.email,
       name: user.name,
+      roles: dbUser.roles.map(role => role.name) 
     };
 
     const jwt = this.jwtService.sign(payload, {
       expiresIn: '30d',
     });
 
-    return { jwt };
+    const jwtCookie = cookie.serialize('jwt', jwt, {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60, 
+      domain: appDomain,
+      path: '/',
+      secure: true,
+    });
+
+
+    return { jwt, jwtCookie};
   }
 }
