59b96eef469ec7b5f0c60a2fc1a68e866b0e7bc4
Shevrin Pavel <vestax747@gmail.com>
2024-09-05 22:08:19 +0300

    [AP-81] enable cors for localhost

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,11 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GatewayModule } from './gateway.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
-  const gatewayPort = app.get(ConfigService).get('GATEWAY_PORT');
-
+  const app = await NestFactory.create(GatewayModule, {
+    logger: ['error', 'log'],
+  });
+  const gatewayPort = await app.get(ConfigService).get('GATEWAY_PORT');
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
 
@@ -19,9 +21,19 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
+  app.enableCors({
+    origin: /^(http:\/\/localhost|http:\/\/127\.0\.0\.1)(:\d+)?$/,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(gatewayPort);
+  await app.listen(gatewayPort || 3000);
+
+  Logger.log(
+    `🚀 Application GATEWAY is running on: http://localhost:${gatewayPort}`
+  );
 }
 
 bootstrap();
