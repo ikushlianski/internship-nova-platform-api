e1d452fc95e2d9459691b88f53743f904fbcc11d
veraivanova233 <veraivanova233@gmail.com>
2024-09-11 18:59:58 +0200

    Update deck.service.ts

--- apps/deck/deck.service.ts
+++ apps/deck/deck.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from 'apps/users/src/prisma/prisma.service';
 import { Deck } from '@prisma/client';
 
 @Injectable()
@@ -36,67 +36,6 @@ export class DeckService {
         title,
         userEmail, 
       },
-    });
-  }
-
-  // Update a deck title by user email and deck ID
-  async updateDeck(
-    userEmail: string,
-    deckId: number,
-    title: string,
-  ): Promise<Deck> {
-    const user = await this.prisma.user.findUnique({
-      where: { email: userEmail },
-    });
-
-    if (!user) {
-      throw new NotFoundException(`User with email ${userEmail} not found`);
-    }
-
-    const existingDeck = await this.prisma.deck.findUnique({
-      where: {
-        id: parseInt(deckId.toString()),
-        userEmail, 
-      },
-    });
-
-    if (!existingDeck) {
-      throw new NotFoundException(
-        `Deck with ID ${deckId} not found for user ${userEmail}`,
-      );
-    }
-
-    return this.prisma.deck.update({
-      where: { id: parseInt(deckId.toString()) },
-      data: { title },
-    });
-  }
-
-  // Delete a deck by user email and deck ID
-  async deleteDeck(userEmail: string, deckId: number): Promise<Deck> {
-    const user = await this.prisma.user.findUnique({
-      where: { email: userEmail },
-    });
-
-    if (!user) {
-      throw new NotFoundException(`User with email ${userEmail} not found`);
-    }
-
-    const existingDeck = await this.prisma.deck.findUnique({
-      where: {
-        id: parseInt(deckId.toString()),
-        userEmail, 
-      },
-    });
-
-    if (!existingDeck) {
-      throw new NotFoundException(
-        `Deck with ID ${deckId} not found for user ${userEmail}`,
-      );
-    }
-
-    return this.prisma.deck.delete({
-      where: { id: parseInt(deckId.toString()) },
     });
   }
 }
